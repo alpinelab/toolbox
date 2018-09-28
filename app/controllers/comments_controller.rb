34a53ee87384732b8178638fbe6ab72db879801b
Thibault Jouan <tj@a13.fr>
2018-09-28 13:00:39 +0000

    Remove comments from controllers

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,26 +3,18 @@
 class CommentsController < ApplicationController
   before_action :set_comment, only: %i[show edit update destroy]
 
-  # GET /comments
-  # GET /comments.json
   def index
     @comments = Comment.all
   end
 
-  # GET /comments/1
-  # GET /comments/1.json
   def show; end
 
-  # GET /comments/new
   def new
     @comment = Comment.new
   end
 
-  # GET /comments/1/edit
   def edit; end
 
-  # POST /comments
-  # POST /comments.json
   def create
     @comment = Comment.new(comment_params)
 
@@ -45,8 +37,6 @@ class CommentsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /comments/1
-  # PATCH/PUT /comments/1.json
   def update
     respond_to do |format|
       if @comment.update(comment_params)
@@ -67,8 +57,6 @@ class CommentsController < ApplicationController
     end
   end
 
-  # DELETE /comments/1
-  # DELETE /comments/1.json
   def destroy
     @comment.destroy
     respond_to do |format|
@@ -79,12 +67,10 @@ class CommentsController < ApplicationController
 
 private
 
-  # Use callbacks to share common setup or constraints between actions.
   def set_comment
     @comment = Comment.find(params[:id])
   end
 
-  # Never trust parameters from the scary internet, only allow the white list through.
   def comment_params
     params.require(:comment).permit(:body, :published, :dependency_id)
   end
--- app/controllers/dependencies_controller.rb
+++ app/controllers/dependencies_controller.rb
@@ -3,26 +3,18 @@
 class DependenciesController < ApplicationController
   before_action :set_dependency, only: %i[show edit update destroy]
 
-  # GET /dependencies
-  # GET /dependencies.json
   def index
     @dependencies = Dependency.all
   end
 
-  # GET /dependencies/1
-  # GET /dependencies/1.json
   def show; end
 
-  # GET /dependencies/new
   def new
     @dependency = Dependency.new
   end
 
-  # GET /dependencies/1/edit
   def edit; end
 
-  # POST /dependencies
-  # POST /dependencies.json
   def create
     @dependency = Dependency.new(dependency_params)
     respond_to do |format|
@@ -45,8 +37,6 @@ class DependenciesController < ApplicationController
     end
   end
 
-  # PATCH/PUT /dependencies/1
-  # PATCH/PUT /dependencies/1.json
   def update
     respond_to do |format|
       if @dependency.update(dependency_params)
@@ -68,8 +58,6 @@ class DependenciesController < ApplicationController
     end
   end
 
-  # DELETE /dependencies/1
-  # DELETE /dependencies/1.json
   def destroy
     @dependency.destroy
     respond_to do |format|
@@ -85,13 +73,10 @@ class DependenciesController < ApplicationController
 
 private
 
-  # Use callbacks to share common setup or constraints between actions.
   def set_dependency
     @dependency = Dependency.find(params[:id])
   end
 
-  # Never trust parameters from the scary internet,
-  # only allow the white list through.
   def dependency_params
     params.require(:dependency).permit(:name)
   end
